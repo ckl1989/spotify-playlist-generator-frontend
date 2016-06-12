e707d5e519a0bd9fb02b925a9192fac80f019c3e
Chris Lam <christopher.kong.lam@gmail.com>
2016-06-12 00:19:29 -0700

    brought in spotify web api and hooked up searchTracks dummy functionality to see if I am able to connect to the web api. There is an issue with importing the api source file so right now, I've set the object as a var in the artist controller class

---
+++ app/adapters/application.js
@@ -0,0 +1,7 @@
+import JSONAPIAdapter from 'ember-data/adapters/json-api';
+
+export default JSONAPIAdapter.extend({
+  // pathForType: function(type) {
+  //   return Ember.String.pluralize(Ember.String.underscore(type));
+  // }
+});
